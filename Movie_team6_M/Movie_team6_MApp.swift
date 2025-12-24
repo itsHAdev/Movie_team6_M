fa2c518ad275ab49cafc445c475607e7a41f31a7
Hadeel <ha.ada1@hotmail.com>
2025-12-24 10:03:59 +0300

    Initial Commit

---
+++ Movie_team6_M/Movie_team6_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_team6_MApp.swift
+//  Movie_team6_M
+//
+//  Created by Hadeel Alansari on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Movie_team6_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
